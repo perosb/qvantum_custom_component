8c9c7cd3f60488c520ce17e04f8152f35c9d3a7d
Per Osbäck <perosb@users.noreply.github.com>
2025-03-16 19:09:46 +0100

    Add validation hassfest/hacs action (#1)
    * add hassfest action
    * add hacs action

--- custom_components/qvantum/__init__.py
+++ custom_components/qvantum/__init__.py
@@ -63,6 +63,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: MyConfigEntry) ->
         },
         manufacturer=coordinator.data.get("device").get("vendor"),
         model=coordinator.data.get("device").get("model"),
+        translation_key="qvantum_flvp",
         name="Qvantum",
         serial_number=coordinator.data.get("device").get("serial"),
         sw_version=f"{coordinator.data.get('device_metadata').get('display_fw_version')}/{coordinator.data.get('device_metadata').get('cc_fw_version')}/{coordinator.data.get('device_metadata').get('inv_fw_version')}",
