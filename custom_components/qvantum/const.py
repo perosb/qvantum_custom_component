8e5a4a34ce3c1627c0d1d0e5fe93f00764d4490c
Per Osbeck <per.osbeck@consid.se>
2025-03-17 19:14:46 +0100

    Reduce poll interval 90s

--- custom_components/qvantum/const.py
+++ custom_components/qvantum/const.py
@@ -1,6 +1,6 @@
 """Constants for the Qvantum Heat Pump Integration."""
 
 DOMAIN = "qvantum"
-DEFAULT_SCAN_INTERVAL = 60
+DEFAULT_SCAN_INTERVAL = 90
 MIN_SCAN_INTERVAL = 10
 SETTING_UPDATE_APPLIED= "APPLIED"
