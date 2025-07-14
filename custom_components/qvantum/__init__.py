cfb547d8a83306e7fbac4b23b62b9ed2d5f9b59b
Per Osbäck <perosb@users.noreply.github.com>
2025-07-14 19:35:10 +0200

    Use internal api for more and up to date metrics (#4)
    ** DANGER WILL ROBINSSON **
    
    Use internal API for all metrics
    Possible breaking change as metrics have moved been renamed (it will try to migrate).
    This API is used by the app.
    Given that there is a 3-6s latency, this API might pull live metrics from flvp.

--- custom_components/qvantum/__init__.py
+++ custom_components/qvantum/__init__.py
@@ -62,19 +62,19 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: MyConfigEntry) ->
     coordinator = QvantumDataUpdateCoordinator(hass, config_entry)
     await coordinator.async_config_entry_first_refresh()
 
-    if not coordinator.data.get('device') or not coordinator.data.get('device_metadata'):
+    if not coordinator.data.get('device') or not coordinator.data.get('device').get('device_metadata'):
         _LOGGER.error("No device data found when setting up Qvantum integration, 2nd attempt")
         await coordinator.async_config_entry_first_refresh()
 
-    if not coordinator.data.get('device') or not coordinator.data.get('device_metadata'):
+    if not coordinator.data.get('device') or not coordinator.data.get('device').get('device_metadata'):
         _LOGGER.error("No device data found when setting up Qvantum integration, failure")
         return False
-
 
     config_entry.async_on_unload(
         config_entry.add_update_listener(_async_update_listener)
     )
 
+    device_metadata = coordinator.data.get('device').get('device_metadata')
     device = DeviceInfo(
         identifiers={
             (DOMAIN, f"qvantum-{coordinator.data.get('device').get('id')}"),
@@ -84,7 +84,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: MyConfigEntry) ->
         translation_key="qvantum_flvp",
         name="Qvantum",
         serial_number=coordinator.data.get("device").get("serial"),
-        sw_version=f"{coordinator.data.get('device_metadata').get('display_fw_version')}/{coordinator.data.get('device_metadata').get('cc_fw_version')}/{coordinator.data.get('device_metadata').get('inv_fw_version')}",
+        sw_version=f"{device_metadata.get('display_fw_version')}/{device_metadata.get('cc_fw_version')}/{device_metadata.get('inv_fw_version')}",
     )
 
     #await async_setup_services(hass)
@@ -111,12 +111,16 @@ async def async_unload_entry(hass: HomeAssistant, config_entry: MyConfigEntry) -
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
 
+# Example migration function
 async def async_migrate_entry(hass, config_entry: ConfigEntry):
     """Migrate old entry."""
     _LOGGER.debug("Migrating configuration from version %s.%s", config_entry.version, config_entry.minor_version)
 
     if config_entry.version > 1:
-        
+        return False
+
+    if config_entry.version == 1:
+
         @callback
         def update_unique_id(entity_entry):
             """Update unique ID of entity entry."""
@@ -127,14 +131,14 @@ async def async_migrate_entry(hass, config_entry: ConfigEntry):
 
             old_unique_id = entity_entry.unique_id
             new_unique_id = entity_entry.unique_id
-            new_unique_id = new_unique_id.replace("_bt1_", "_outdoor_temperature_")
-            new_unique_id = new_unique_id.replace("_bt2_", "_indoor_temperature_")
-            new_unique_id = new_unique_id.replace("_cal_heat_temp_", "_heating_flow_temperature_target_")
-            new_unique_id = new_unique_id.replace("_bt11_", "_heating_flow_temperature_")
-            new_unique_id = new_unique_id.replace("_bt30_", "_tap_water_tank_temperature_")
-            new_unique_id = new_unique_id.replace("_tap_water_cap", "_tap_water_capacity")
-            new_unique_id = new_unique_id.replace("_dhw_normal_start_", "_tap_water_start_")
-            new_unique_id = new_unique_id.replace("_dhw_normal_stop_", "_tap_water_stop_")
+            new_unique_id = new_unique_id.replace("_outdoor_temperature_", "_bt1_")
+            new_unique_id = new_unique_id.replace("_indoor_temperature_", "_bt2_")
+            new_unique_id = new_unique_id.replace("_heating_flow_temperature_target_", "_cal_heat_temp_")
+            new_unique_id = new_unique_id.replace("_heating_flow_temperature_", "_bt11_")
+            new_unique_id = new_unique_id.replace("_tap_water_tank_temperature_", "_bt30_")
+            new_unique_id = new_unique_id.replace("_tap_water_capacity", "_tap_water_cap")
+            new_unique_id = new_unique_id.replace("_tap_water_start_", "_dhw_normal_start_")
+            new_unique_id = new_unique_id.replace("_tap_water_stop_", "_dhw_normal_stop_")
 
             if old_unique_id == new_unique_id:
                 return None
